42d6d4bb95813d009efbbd259ad67689937ef42a
Obirijah Tochukwu <tobirijah@gmail.com>
2021-12-14 15:00:36 +0000

    nue

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<React.StrictMode>
+    <AppProvider>
+      <Router>
+        <App />
+      </Router>
+    </AppProvider>
+  </React.StrictMode>,document.getElementById('root'));
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
